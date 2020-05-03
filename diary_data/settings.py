32cc037bd4287c51f91662b5290996c983a7c822
gouthamravella <diaryproject2019@gmail.com>
2020-05-03 18:59:28 +0530

    ...

--- diary_data/settings.py
+++ diary_data/settings.py
@@ -91,9 +91,9 @@ if os.getenv('PYTHONANYWHERE_DOMAIN', '').startswith('pythonanywhere'):
             'PASSWORD':'Test@1234',
             # 'PORT':3307,
             'OPTIONS': {
-                'init_command': 'SET default_storage_engine=INNODB',
-                'sql_mode': 'STRICT_TRANS_TABLES',
-                # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                # 'init_command': 'SET default_storage_engine=INNODB',
+                # 'sql_mode': 'STRICT_TRANS_TABLES',
                 # 'read_default_file': os.path.join(BASE_DIR, 'migo_django/mysql.cnf'),
             },
             # Tell Django to build the test database with the 'utf8mb4' character set
